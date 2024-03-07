30a6be2acd652e4b0f45b8f508ab652e83f9b3e5
Anna Spjut <anna.spjut@gmail.com>
2024-03-07 19:00:39 +0400

    [add] Snack 1

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// /Snack 1///
+// 1. creo un array di objects
+// 2. con forEach calcolo la somma del peso degli objects
+
+const zucchine = [
+  {
+    nome:'Zucchina 1',
+    varietà: 'verde',
+    peso: 200,
+    lunghezza: 10
+  },
+  {
+    nome:'Zucchina 2',
+    varietà: 'tonda',
+    peso: 140,
+    lunghezza: 8
+  },
+  {
+    nome:'Zucchina 3',
+    varietà: 'siciliana',
+    peso: 80,
+    lunghezza: 24
+  },
+  {
+    nome:'Zucchina 4',
+    varietà: 'gialla',
+    peso: 95,
+    lunghezza: 18
+  },
+  {
+    nome:'Zucchina 5',
+    varietà: 'romanesca',
+    peso: 175,
+    lunghezza: 25
+  },
+  {
+    nome:'Zucchina 6',
+    varietà: 'fiorentina',
+    peso: 190,
+    lunghezza: 12
+  },
+  {
+    nome:'Zucchina 7',
+    varietà: 'trombetta',
+    peso: 95,
+    lunghezza: 19
+  },
+  {
+    nome:'Zucchina 8',
+    varietà: 'napoletana',
+    peso: 120,
+    lunghezza: 29
+  },
+  {
+    nome:'Zucchina 9',
+    varietà: 'chiara',
+    peso: 80,
+    lunghezza: 26
+  },
+  {
+    nome:'Zucchina 10',
+    varietà: 'nera',
+    peso: 85,
+    lunghezza: 9
+  }
+];
+
+let sum = 0
+
+zucchine.forEach(element => {
+  sum += element.peso;
+});
+
+console.log(sum);
+
+
